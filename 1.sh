f7cb5e3d56210b5b2f84078529798d972664bc48
Anton Patsev <a.patsev@sxope.com>
2022-06-30 10:44:39 +0600

    fix(ui): broken /security/activity-log (BASE-1469)
    - disable ajax timing

--- 1.sh
+++ 1.sh
@@ -1,4 +1,15 @@
 #!/usr/bin/env bash
 
+source ".env.commit_message_regexp"
+set -o xtrace  # debug
 message="fix(ui): broken /security/activity-log (BASE-1469)\n\n- disable ajax timing"
 
+readarray -t $message
+for MESSAGE_COMMITS in "${LIST_MESSAGE_COMMITS[@]}"
+do
+  if ! [[ ${MESSAGE_COMMITS} =~ ${COMMIT_MESSAGE_REGEXP} ]]; then
+    echo "Invalid message commit:"
+    echo "${MESSAGE_COMMITS}"
+    exit 1
+  fi
+done
